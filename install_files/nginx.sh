da2c793ccb572327b7ffacc2911f83c45ca41dde
Scott Stiffler <scotts@rainks.com>
2013-05-25 01:12:57 -0500

    Add group to nginx.conf

--- install_files/nginx.sh
+++ install_files/nginx.sh
@@ -56,6 +56,6 @@ install_nginx() {
 }' > /etc/logrotate.d/nginx
 
     # Add deploy user
-    useradd deploy --shell /bin/bash --ingroup www-data
+    useradd deploy --shell /bin/bash --group www-data
 }
 
